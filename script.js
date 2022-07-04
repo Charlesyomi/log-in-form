ad1853e211d811be04985ff02943c87e032a92d8
charles <yomiadesoba@gmail.com>
2022-07-04 12:36:58 +0100

    improved the forms help text

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-// if the browser's javascript is enable disable html validity
+
 
 const logInForm = document.getElementById("log-in-form")
-
 const formInputs = document.getElementsByTagName('input')
-// console.log(formInputs)
-
 const errorContainer = document.querySelectorAll('input + span.error')
-console.log(errorContainer)
+
+// if the browser's javascript is enable disable html validity
 logInForm.setAttribute("novalidate","novalidate")
 
 
@@ -21,7 +19,6 @@ logInForm.addEventListener("submit",function(event){
             errorContainer[i].className='active';
             showError(i);
             errorCheck.push("error");
-            // console.log(errorCheck)
         }
     };
 
@@ -30,12 +27,28 @@ logInForm.addEventListener("submit",function(event){
     }
 })
 
+function capitalize(a){
+    // split the string into an array
+    const arr = a.split(" ");
+    // capitalize the first character of each item of the array
+    for(let i= 0;i<arr.length;i++){
+        arr[i]= arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    };
+    // return the capitalized string
+    return arr.join(' ');
+}
+
 function showError(i){
     if(formInputs[i].validity.valueMissing){
-        errorContainer[i].textContent='This field cannot be empty';
+        errorContainer[i].textContent=`${capitalize(formInputs[i].getAttribute('name').replace('-',' '))} cannot be empty`;
+        formInputs[i].placeholder = '';
     }
-    else if(formInputs[i].validity.patternMismatch){
-        errorContainer[i].textContent='You have not matched the required pattern';
+    else if(formInputs['email-address'].validity.typeMismatch){
+        errorContainer[2].textContent = 'Look like this is not an email';
+        formInputs['email-address'].value = '';
+        formInputs['email-address'].placeholder = 'email@example/com';
+        // formInputs['email-address'].setAttribute('placeholder','abc@gmail.com')
+        formInputs['email-address'].classList.add('error-type-placeholder')
+                
     }
-
 }
